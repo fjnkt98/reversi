20d2ad0d98fc78b1af0199edb657ef18c783cccf
fjnkt98 <fjnkt98@gmail.com>
2022-10-01 12:38:48 +0900

    Update: エントリーポイントのコマンド名を変更

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1.0",
     install_requires=["numpy"],
     extras_require={"develop": ["mypy", "flake8", "black"]},
-    entry_points={"console_scripts": {"app = reversi.reversi:main"}},
+    entry_points={"console_scripts": {"reversi = reversi.reversi:main"}},
 )
